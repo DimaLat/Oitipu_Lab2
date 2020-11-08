c2cc6843be10bd00770a7ee0ecc50dacfad1b052
DimaLat <dima.latyshionok@gmail.com>
2020-11-08 19:07:54 +0300

    init commit

---
+++ app/src/main/java/com/example/vito/paint/Tool.java
@@ -0,0 +1,10 @@
+package com.example.vito.paint;
+
+
+public class Tool {
+    public static final int BRUSH     = 1;
+    public static final int RECTANGLE = 2;
+    public static final int OVAL      = 3;
+    public static final int TEXT      = 4;
+    public static final int MOVE      = 5;
+}
